ef988a528ef1de2eccbddf48f8e609963f89db5c
iquesadmor <iquesadmor@educacion.navarra.es>
2025-12-19 08:38:09 +0100

    Constructor añadido

--- Examen_ivan/src/Examen/Vehiculo.java
+++ Examen_ivan/src/Examen/Vehiculo.java
@@ -2,4 +2,16 @@ package Examen;
 
 public class Vehiculo {
 
+	private String Matricula;
+	private String Marca;
+	private String Modelo;
+	private String Estado;
+	
+
+    public Vehiculo(String matricula, String marca, String modelo, String estado) {
+        this.Matricula = matricula;
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.Estado = estado;
+	
 }
